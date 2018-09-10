7acf2ecf753ab46994818d372b07a37f753daaab
dominiceggerman <dominiceggerman@gmail.com>
2018-09-10 11:36:47 -0500

    More work on Pandas demo

--- pandas_demo.py
+++ pandas_demo.py
@@ -17,4 +17,13 @@ if __name__ == '__main__':
     # for date, flow in zip(df["Effective Date"].values, df["Scheduled (BZ)"].values):
     #     print(date + " : " + str(flow))
 
+    # # Change up columns
+    # mmcf = df.loc[:, "Scheduled (BZ)"] / 1030  # Applying scalar to column, makes a series (list)
+    # mmcf = round(mmcf, 2)  # Round to 2 DP
+    # df["Scheduled (MMcf)"] = mmcf  # Adding column to df.  There are different ways to do this
+    # df = df.drop(columns=["Design (BZ)", "Available (BZ)"])  # Drop the design cap column
+    # df = df[["Effective Date", "Scheduled (BZ)", "Scheduled (MMcf)", "Operational (BZ)"]]  # Rearrange the columns
+    # print(df)
     
+    # # Ouput with different column headers and no index
+    # df.to_csv("output_data.csv", header=["Date", "Sched (BZ)", "Sched (MMcf)", "Opcap (BZ)"], index=False)
